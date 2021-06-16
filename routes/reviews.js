8db24ae18dac3e945a5dee1c7ca8bd27c303e2ce
dcao7 <dcao1736@gmail.com>
2021-06-16 19:43:27 -0400

    add reviews permissions

--- routes/reviews.js
+++ routes/reviews.js
@@ -7,11 +7,12 @@ const ExpressError = require('../utils/ExpressError')
 
 const Campground = require('../models/campground');
 const Review = require('../models/review');
-const { validateReview } = require('../middleware');
+const { validateReview, isLoggedIn } = require('../middleware');
 
-router.post('/', validateReview, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user.id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
