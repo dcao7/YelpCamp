c3a70c06b209cddc499e03437a2c10a8ce0a1081
dcao7 <dcao1736@gmail.com>
2021-06-17 16:57:01 -0400

    add reviews authorization

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 const ExpressError = require('./utils/ExpressError')
 const { campgroundSchema, reviewSchema } = require('./schemas')
 
@@ -40,4 +41,14 @@ module.exports.validateReview = (req, res, next) => {
     } else {
         next();
     }
+}
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if (!review.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
 }
